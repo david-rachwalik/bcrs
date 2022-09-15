62133871a4554cf21f6d2fbe3dcab02fff7aabe2
jhartung <joel.hartung@gmail.com>
2022-09-14 19:45:10 -0500

    Added DB connection information

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express(); // Express variable.
  * App configurations.
  */
 app.use(express.json());
-app.use(express.urlencoded({'extended': true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../dist/bcrs')));
 app.use('/', express.static(path.join(__dirname, '../dist/bcrs')));
 
@@ -19,18 +19,22 @@ app.use('/', express.static(path.join(__dirname, '../dist/bcrs')));
 const PORT = 3000 || process.env.PORT;
 
 // TODO: This line will be replaced with your database connection string (including username/password).
-const CONN = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/nodebucket?retryWrites=true&w=majority';
+const CONN =
+  'mongodb+srv://bcrs_admin:s3cret@cluster0.580azmi.mongodb.net/bcrs?retryWrites=true&w=majority'; // database string added - JVH 9/14/2022
 
 /**
  * Database connection.
  */
-mongoose.connect(CONN).then(() => {
-  console.log('Connection to the database was successful');
-}).catch(err => {
-  console.log('MongoDB Error: ' + err.message);
-});
+mongoose
+  .connect(CONN)
+  .then(() => {
+    console.log('Connection to the database was successful');
+  })
+  .catch((err) => {
+    console.log('MongoDB Error: ' + err.message);
+  });
 
 // Wire-up the Express server.
 app.listen(PORT, () => {
   console.log('Application started and listening on PORT: ' + PORT);
-})
+});
