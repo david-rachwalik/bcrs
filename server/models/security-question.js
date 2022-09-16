432328b2ba9897b004c60cf22975e03c70a42cca
David Rachwalik <david.rachwalik@outlook.com>
2022-09-15 20:15:58 -0500

    added securityQuestionSchema

--- server/models/security-question.js
+++ server/models/security-question.js
@@ -1,8 +1,22 @@
 /*
 ============================================
 ; Title: Bob's Computer Repair Shop (Sprint 1)
-; Author: Professor Krasso
+; Author: David Rachwalik
 ; Date: 15 September 2022
 ; Modified By: Joel Hartung, Allan Trejo, David Rachwalik
 ;===========================================
 */
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const securityQuestionSchema = new Schema(
+  {
+    text: { type: String },
+    isDisabled: { type: Boolean, default: false },
+  },
+  { collection: 'securityQuestions' },
+);
+
+module.exports = mongoose.model('SecurityQuestion', securityQuestionSchema);
