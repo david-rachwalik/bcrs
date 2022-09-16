918c9099b36724767fcafa9c1a241c9e86bce1a0
jhartung <joel.hartung@gmail.com>
2022-09-16 11:18:23 -0500

    findAllSecurityQuestions service update

--- src/app/shared/security-question.service.ts
+++ src/app/shared/security-question.service.ts
@@ -1,9 +1,25 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop (Sprint 1)
+; Author: Professor Krasso
+; Date: 16 September 2022
+; Modified By: Joel Hartung, Allan Trejo, David Rachwalik
+; Description:  Service layer for Security Question Documents
+;===========================================
+*/
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+// import { SecurityQuestion } from './security-question.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SecurityQuestionService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  findAllSecurityQuestions(): Observable<any> {
+    return this.http.get('/api/security-questions');
+  }
 }
