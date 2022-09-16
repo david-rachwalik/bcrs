9804c618315d18d58f142e7569719b64c8b205ae
jhartung <joel.hartung@gmail.com>
2022-09-16 12:58:45 -0500

    app-routing & authguard updates

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,15 +1,43 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop (Sprint 1)
+; Author: Professor Krasso
+; Date: 16 September 2022
+; Modified By: Joel Hartung, Allan Trejo, David Rachwalik
+;===========================================
+*/
+
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private cookieService: CookieService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot,
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const isAuthenticated = this.cookieService.get('sessionuser');
+
+    if (isAuthenticated) {
+      return true;
+    }
+    this.router.navigate(['/session/signin']);
+    return false;
   }
-  
 }
