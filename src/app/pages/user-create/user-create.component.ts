bccefa7eb122f4ecd3c35ffa6fe19b3ea97e473c
David Rachwalik <david.rachwalik@outlook.com>
2022-09-18 16:13:52 -0500

    initial user create page

--- src/app/pages/user-create/user-create.component.ts
+++ src/app/pages/user-create/user-create.component.ts
@@ -1,15 +1,77 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop (Sprint 1)
+; Author: Professor Krasso
+; Date: 18 September 2022
+; Modified By: Joel Hartung, Allan Trejo, David Rachwalik
+; Description: User create component
+;===========================================
+*/
+
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../shared/user.interface';
+import { UserService } from '../../shared/user.service';
 
 @Component({
-  selector: 'app-user-create',
+  selector: 'app-use-create',
   templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.scss']
+  styleUrls: ['./user-create.component.scss'],
 })
 export class UserCreateComponent implements OnInit {
+  // Use FormGroup to define valid values and capture input
+  userForm: FormGroup = this.fb.group({
+    userName: [null, Validators.compose([Validators.required])],
+    password: [
+      null,
+      Validators.compose([
+        Validators.required,
+        // Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$'),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z]).{8,}$'),
+      ]),
+    ],
+    firstName: [null, Validators.compose([Validators.required])],
+    lastName: [null, Validators.compose([Validators.required])],
+    phoneNumber: [null, Validators.compose([Validators.required])],
+    address: [null, Validators.compose([Validators.required])],
+    email: [null, Validators.compose([Validators.required])],
+  });
+  // user!: User;
+  // userId!: string;
+  // roles: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private userService: UserService,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createUser(): void {
+    const newUser: User = {
+      userName: this.userForm.controls['userName'].value,
+      password: this.userForm.controls['password'].value,
+      firstName: this.userForm.controls['firstName'].value,
+      lastName: this.userForm.controls['lastName'].value,
+      phoneNumber: this.userForm.controls['phoneNumber'].value,
+      address: this.userForm.controls['address'].value,
+      email: this.userForm.controls['email'].value,
+    };
+    console.log(`New user: ${newUser}`);
+
+    this.userService.createUser(newUser).subscribe({
+      next: (res) => {
+        this.router.navigate(['/users']);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
+  cancel(): void {
+    this.router.navigate(['/users']);
+  }
 }
