c09bfa0acdc1c3364151abea3a4293de88d3994e
David Rachwalik <david.rachwalik@outlook.com>
2022-09-18 14:57:20 -0500

    updated subscribe method and code attributions

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -1,70 +1,86 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop (Sprint 1)
+; Author: Professor Krasso
+; Date: 18 September 2022
+; Modified By: Joel Hartung, Allan Trejo, David Rachwalik
+; Description: User details component
+;===========================================
+*/
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from 'src/app/shared/user.service';
 import { User } from 'src/app/shared/user.interface';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: [ './user-details.component.scss' ]
+  styleUrls: ['./user-details.component.scss'],
 })
-
-
 export class UserDetailsComponent implements OnInit {
   user!: User;
   userId!: string;
   form!: FormGroup;
   roles: any;
 
-  constructor (private route: ActivatedRoute, private fb: FormBuilder, private router: Router, private userService: UserService) {
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+  ) {
     this.userId = this.route.snapshot.paramMap.get('userId')!;
+    // commented out due to error message? - Type 'string | null' is not assignable to type 'string'
 
-    this.userService.findUserById(this.userId).subscribe(res => {
-      this.user = res[ 'data' ];
-    }, err => {
-      console.log(err);
-    }, () => {
-      this.form.controls[ 'firstName' ].setValue(this.user.firstName);
-      this.form.controls[ 'lastName' ].setValue(this.user.lastName);
-      this.form.controls[ 'phoneNumber' ].setValue(this.user.phoneNumber);
-      this.form.controls[ 'address' ].setValue(this.user.address);
-      this.form.controls[ 'email' ].setValue(this.user.email);
+    this.userService.findUserById(this.userId).subscribe({
+      next: (res) => {
+        this.user = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        this.form.controls['firstName'].setValue(this.user.firstName);
+        this.form.controls['lastName'].setValue(this.user.lastName);
+        this.form.controls['phoneNumber'].setValue(this.user.phoneNumber);
+        this.form.controls['address'].setValue(this.user.address);
+        this.form.controls['email'].setValue(this.user.email);
+      },
     });
   }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      firstName: [ null, Validators.compose([ Validators.required ]) ],
-      lastName: [ null, Validators.compose([ Validators.required ]) ],
-      phoneNumber: [ null, Validators.compose([ Validators.required ]) ],
-      address: [ null, Validators.compose([ Validators.required ]) ],
-      email: [ null, Validators.compose([ Validators.required ]) ]
-
+      firstName: [null, Validators.compose([Validators.required])],
+      lastName: [null, Validators.compose([Validators.required])],
+      phoneNumber: [null, Validators.compose([Validators.required])],
+      address: [null, Validators.compose([Validators.required])],
+      email: [null, Validators.compose([Validators.required])],
     });
   }
 
   saveUser(): void {
     const updatedUser: User = {
-      firstName: this.form.controls[ 'firstName' ].value,
-      lastName: this.form.controls[ 'lastName' ].value,
-      phoneNumber: this.form.controls[ 'phoneNumber' ].value,
-      address: this.form.controls[ 'address' ].value,
-      email: this.form.controls[ 'email' ].value
+      firstName: this.form.controls['firstName'].value,
+      lastName: this.form.controls['lastName'].value,
+      phoneNumber: this.form.controls['phoneNumber'].value,
+      address: this.form.controls['address'].value,
+      email: this.form.controls['email'].value,
     };
 
     this.userService.updateUser(this.userId, updatedUser).subscribe({
       next: (res: any) => {
-        this.router.navigate([ '/users' ]);
+        this.router.navigate(['/users']);
       },
       error: (e: any) => {
         console.log(e);
-      }
+      },
     });
-
   }
 
   cancel(): void {
-    this.router.navigate([ '/users' ]);
+    this.router.navigate(['/users']);
   }
 }
--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,46 +1,68 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop (Sprint 1)
+; Author: Professor Krasso
+; Date: 18 September 2022
+; Modified By: Joel Hartung, Allan Trejo, David Rachwalik
+; Description: User list component
+;===========================================
+*/
+
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteRecordDialogComponent } from '../../shared/delete-record-dialog/delete-record-dialog.component';
-import { UserService } from '../../shared/user.service';
 import { User } from '../../shared/user.interface';
+import { UserService } from '../../shared/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: [ './user-list.component.scss' ]
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
   users: User[] = [];
-  constructor (private dialog: MatDialog, private UserService: UserService) {
-    this.UserService.findAllUsers().subscribe(res => {
-      this.users = res[ 'data' ];
-      console.log(this.users);
-    }, err => {
-      console.log(err);
+  constructor(private dialog: MatDialog, private UserService: UserService) {
+    this.UserService.findAllUsers().subscribe({
+      next: (res) => {
+        this.users = res.data;
+        console.log(this.users);
+      },
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   delete(userId: string, recordId: string): void {
     const dialogRef = this.dialog.open(DeleteRecordDialogComponent, {
       data: {
         recordId,
-        dialogHeader: "Delete Record Dialog",
-        dialogBody: `Are you sure you want to delete user ${recordId}?`
+        dialogHeader: 'Delete Record Dialog',
+        dialogBody: `Are you sure you want to delete user ${recordId}?`,
       },
       disableClose: true,
-      width: '800px'
+      width: '800px',
     });
 
-    dialogRef.afterClosed().subscribe(res => {
-      if (res === 'confirm') {
-        this.UserService.deleteUser(userId).subscribe(res => {
-          console.log('User Deleted');
-          this.users = this.users.filter(u => u._id !== userId);
-        });
-      }
+    dialogRef.afterClosed().subscribe({
+      next: (res) => {
+        if (res === 'confirm') {
+          this.UserService.deleteUser(userId).subscribe({
+            next: (res) => {
+              console.log('User Deleted');
+              this.users = this.users.filter((u) => u._id !== userId);
+            },
+            error: (err) => {
+              console.log(err);
+            },
+          });
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
 }
