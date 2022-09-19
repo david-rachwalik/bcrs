747a3b384f07b1813fff6f8c82c60c4562ff59fa
Arkenstone <trejo.allan@yahoo.com>
2022-09-18 17:03:56 -0700

    Commit create security question

--- src/app/pages/security-question-create/security-question-create.component.ts
+++ src/app/pages/security-question-create/security-question-create.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SecurityQuestionService } from 'src/app/shared/services/security-question.service';
+import { SecurityQuestion } from 'src/app/shared/interfaces/security-question.interface';
 
 @Component({
   selector: 'app-security-question-create',
   templateUrl: './security-question-create.component.html',
-  styleUrls: ['./security-question-create.component.scss']
+  styleUrls: [ './security-question-create.component.scss' ]
 })
 export class SecurityQuestionCreateComponent implements OnInit {
+  form: FormGroup = this.fb.group({
+    text: [ null, Validators.compose([ Validators.required ]) ]
+  });
 
-  constructor() { }
+  constructor (private fb: FormBuilder, private router: Router, private securityQuestionService: SecurityQuestionService) { }
 
   ngOnInit(): void {
+
   }
 
+  create(): void {
+    const newSecurityQuestion: SecurityQuestion = {
+      text: this.form.controls[ 'text' ].value,
+    };
+
+    this.securityQuestionService.createSecurityQuestion(newSecurityQuestion).subscribe({
+      next: (res: any) => {
+        this.router.navigate([ '/security-questions' ]);
+      },
+      error: (e: any) => {
+        console.log(e);
+      }
+    });
+  }
+  cancel(): void {
+    this.router.navigate([ '/security-questions' ]);
+  }
 }
