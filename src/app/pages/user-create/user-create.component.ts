037eb932df96235e77308a1db817b5391c21838d
David Rachwalik <david.rachwalik@outlook.com>
2022-09-19 16:38:07 -0500

    fix for validation pattern and message

--- src/app/pages/user-create/user-create.component.ts
+++ src/app/pages/user-create/user-create.component.ts
@@ -28,8 +28,7 @@ export class UserCreateComponent implements OnInit {
       null,
       Validators.compose([
         Validators.required,
-        // Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$'),
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z]).{8,}$'),
+        Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$'),
       ]),
     ],
     firstName: [null, Validators.compose([Validators.required])],
