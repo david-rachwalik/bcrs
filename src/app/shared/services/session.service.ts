704b4c36d97b13037e48a44cc97e69e86f7476e5
David Rachwalik <david.rachwalik@outlook.com>
2022-09-25 19:18:46 -0500

    corrected modelText3 to answerText3

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -1,22 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { UrlSegment } from '@angular/router';
-import { User } from '../interfaces/user.interface';
 import { Observable } from 'rxjs';
+import { User } from '../interfaces/user.interface';
 import { VerifySecurityQuestionModel } from '../interfaces/verify-security-question.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SessionService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  login(userName: string, password:string): Observable<any> {
+  login(userName: string, password: string): Observable<any> {
     return this.http.post('/api/session/login', {
       userName,
-      password
-    })
+      password,
+    });
   }
 
   register(user: User): Observable<any> {
@@ -28,28 +26,37 @@ export class SessionService {
       phoneNumber: user.phoneNumber,
       email: user.email,
       address: user.address,
-      selectedSecurityQuestions: user.selectedSecurityQuestions
-    })
+      selectedSecurityQuestions: user.selectedSecurityQuestions,
+    });
   }
 
   verifyUsername(username: string): Observable<any> {
     return this.http.get('/api/session/verify/users/' + username);
   }
 
-  verifySecurityQuestions(model: VerifySecurityQuestionModel, username: string): Observable<any> {
-    return this.http.post('/api/session/verify/users/' + username + '/security-questions', {
-      questionText1: model.question1,
-      questionText2: model.question2,
-      questionText3: model.question3,
-      answerText1: model.answerToQuestion1,
-      answerText2: model.answerToQuestion2,
-      modelText3: model.answerToQuestion3
-    })
+  verifySecurityQuestions(
+    model: VerifySecurityQuestionModel,
+    username: string,
+  ): Observable<any> {
+    return this.http.post(
+      '/api/session/verify/users/' + username + '/security-questions',
+      {
+        questionText1: model.question1,
+        questionText2: model.question2,
+        questionText3: model.question3,
+        answerText1: model.answerToQuestion1,
+        answerText2: model.answerToQuestion2,
+        answerText3: model.answerToQuestion3,
+      },
+    );
   }
 
   updatePassword(password: string, username: string): Observable<any> {
-    return this.http.post('/api/session/users/' + username + '/reset-password', {
-      password
-    })
+    return this.http.post(
+      '/api/session/users/' + username + '/reset-password',
+      {
+        password,
+      },
+    );
   }
 }
