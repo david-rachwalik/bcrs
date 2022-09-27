3e040ee7ada1fa12842626651debb50821e43a5b
David Rachwalik <david.rachwalik@outlook.com>
2022-09-27 16:19:48 -0500

    added attribution & strong type to service

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -1,8 +1,23 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop (Sprint 2)
+; Author: Professor Krasso
+; Date: 26 September 2022
+; Modified By: Joel Hartung, Allan Trejo, David Rachwalik
+; Description:  Service layer for Session documents
+;===========================================
+*/
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user.interface';
 import { VerifySecurityQuestionModel } from '../interfaces/verify-security-question.interface';
+
+import { BaseResponse } from '../interfaces/base-response.interface';
+import { ErrorResponse } from '../interfaces/error-response.interface';
+
+type UserResponse = BaseResponse<User> | ErrorResponse<User>;
 
 @Injectable({
   providedIn: 'root',
@@ -10,15 +25,15 @@ import { VerifySecurityQuestionModel } from '../interfaces/verify-security-quest
 export class SessionService {
   constructor(private http: HttpClient) {}
 
-  login(userName: string, password: string): Observable<any> {
-    return this.http.post('/api/session/login', {
+  login(userName: string, password: string): Observable<UserResponse> {
+    return this.http.post<UserResponse>('/api/session/login', {
       userName,
       password,
     });
   }
 
-  register(user: User): Observable<any> {
-    return this.http.post('/api/session/register', {
+  register(user: User): Observable<UserResponse> {
+    return this.http.post<UserResponse>('/api/session/register', {
       userName: user.userName,
       password: user.password,
       firstName: user.firstName,
@@ -30,16 +45,16 @@ export class SessionService {
     });
   }
 
-  verifyUsername(userName: string): Observable<any> {
-    return this.http.get('/api/session/verify/users/' + userName);
+  verifyUsername(userName: string): Observable<UserResponse> {
+    return this.http.get<UserResponse>(`/api/session/verify/users/${userName}`);
   }
 
   verifySecurityQuestions(
     model: VerifySecurityQuestionModel,
     userName: string,
-  ): Observable<any> {
-    return this.http.post(
-      '/api/session/verify/users/' + userName + '/security-questions',
+  ): Observable<UserResponse> {
+    return this.http.post<UserResponse>(
+      `/api/session/verify/users/${userName}/security-questions`,
       {
         questionText1: model.question1,
         questionText2: model.question2,
@@ -47,12 +62,13 @@ export class SessionService {
         answerText1: model.answerToQuestion1,
         answerText2: model.answerToQuestion2,
         answerText3: model.answerToQuestion3,
-      })
+      },
+    );
   }
 
-  updatePassword(password: string, userName: string): Observable<any> {
-    return this.http.post(
-      '/api/session/users/' + userName + '/reset-password',
+  updatePassword(password: string, userName: string): Observable<UserResponse> {
+    return this.http.post<UserResponse>(
+      `/api/session/users/${userName}/reset-password`,
       {
         password,
       },
