b9b297e63bbc42c915490dc528a8cc50faa7ac2b
jhartung <joel.hartung@gmail.com>
2022-09-29 09:05:54 -0500

    role-api.js FindAll update

---
+++ server/routes/role-api.js
@@ -0,0 +1,52 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop (Sprint 3)
+; Author: Professor Krasso
+; Date: 29 September 2022
+; Modified By: Joel Hartung, Allan Trejo, David Rachwalik
+; Description:  API routes for Role documents
+;===========================================
+*/
+
+// require statements
+const express = require('express');
+// const Role = require('../models/role');
+const User = require('../models/user');
+const ErrorResponse = require('../services/error-response');
+const BaseResponse = require('../services/base-response');
+
+const router = express.Router();
+
+/**
+ * FindAll
+ */
+
+router.get('/', async (req, res) => {
+  try
+  {
+    Role.find({})
+    .where('isDisabled')
+    .equals(false)
+    .exec(function(err, roles)
+    {
+      if (err)
+      {
+        console.log(err);
+        const findAllRolesMongodbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+        res.status(500).send(findAllRolesMongodbErrorResponse.toObject());
+      }
+      else
+      {
+        console.log(roles);
+        const findAllRolesResponse = new BaseResponse('200', 'Query successful', roles);
+        res.json(findAllRolesResponse.toObject());
+      }
+    })
+  }
+  catch (e)
+  {
+    console.log(e);
+    const findAllRolesCatchErrorResponse = new ErrorResponse('500', 'Internal server error', e.message);
+    res.status(500).send(findAllRolesCatchErrorResponse.toObject());
+  }
+});
