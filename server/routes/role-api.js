22977039abd203f357e6619946a05a0e7f212116
David Rachwalik <david.rachwalik@outlook.com>
2022-09-30 01:35:25 -0500

    added createRole method to Role API

--- server/routes/role-api.js
+++ server/routes/role-api.js
@@ -10,12 +10,17 @@
 
 // require statements
 const express = require('express');
-// const Role = require('../models/role');
+const Role = require('../models/role');
 const User = require('../models/user');
 const ErrorResponse = require('../services/error-response');
 const BaseResponse = require('../services/base-response');
+const logResponse = require('../services/log-response');
 
 const router = express.Router();
+
+// -------- API --------
+
+// operations: findAll, createRole, UpdateRole, findById
 
 /**
  * findAll
@@ -33,8 +38,7 @@ const router = express.Router();
  *         description: Server exception
  *       '501':
  *         description: MongoDB exception
- * */
-
+ */
 router.get('/', async (req, res) => {
   try {
     Role.find({})
@@ -67,6 +71,73 @@ router.get('/', async (req, res) => {
       e.message,
     );
     res.status(500).send(findAllRolesCatchErrorResponse.toObject());
+  }
+});
+
+/**
+ * createRole
+ * @openapi
+ * /api/role:
+ *   post:
+ *     tags:
+ *       - Roles
+ *     summary: add a Role document
+ *     description: API for creating a new role document.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - text
+ *             properties:
+ *               text:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: Query successful
+ *       '500':
+ *         description: Server exception
+ *       '501':
+ *         description: MongoDB exception
+ */
+router.post('/', async (req, res) => {
+  try {
+    Role.findOne({ text: req.body.text }, (err, role) => {
+      if (err) {
+        const response = logResponse(501, err);
+        res.status(501).send(response);
+      } else {
+        console.log(`Role: ${role}`);
+
+        if (!role) {
+          // Create a new role
+          const newRole = {
+            text: req.body.text,
+          };
+          Role.create(newRole, (error, role) => {
+            if (error) {
+              const response = logResponse(501, error);
+              res.status(501).send(response);
+            } else {
+              // Successfully created a Role document
+              const response = logResponse(200, role);
+              res.json(response);
+            }
+          });
+        } else {
+          console.log(`Role: ${req.body.text} already exists`);
+          const roleAlreadyExists = new ErrorResponse(
+            '200',
+            `Role: ${req.body.text} already exists.  If you do not see the role in the list, it was disabled.`,
+          );
+          res.status(200).send(roleAlreadyExists.toObject());
+        }
+      }
+    });
+  } catch (err) {
+    const response = logResponse(500, err);
+    res.status(500).send(response);
   }
 });
 
@@ -103,8 +174,7 @@ router.get('/', async (req, res) => {
  *         description: Server Exception.
  *       '501':
  *         description: MongoDB Exception.
- * */
-
+ */
 router.put('/:roleId', async (req, res) => {
   try {
     Role.findOne({ _id: req.params.roleId }, function (err, role) {
@@ -175,7 +245,7 @@ router.put('/:roleId', async (req, res) => {
  *         description: Query successful.
  *       '500':
  *         description: Server Exception.
- * */
+ */
 router.get('/:roleId', async (req, res) => {
   try {
     Role.findOne({ _id: req.params.roleId }, function (err, role) {
@@ -193,4 +263,5 @@ router.get('/:roleId', async (req, res) => {
     res.status(500).send(findRoleByIdCatchError);
   }
 });
+
 module.exports = router;
